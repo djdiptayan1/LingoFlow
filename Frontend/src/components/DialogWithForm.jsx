7c979fd33d050a72dca38a6a8ab8fb30818a94ac
greeenboi <suvan.gowrishanker.204@gmail.com>
2023-09-12 11:55:25 +0530

    added modal

--- Frontend/src/components/DialogWithForm.jsx
+++ Frontend/src/components/DialogWithForm.jsx
@@ -1,126 +1,56 @@
-import React, { useState, useEffect } from "react";
-import menu from "../assets/menu.png";
-import axios from "axios";
+import React from 'react'
 import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
     Button,
-    Dialog,
-    Card,
-    CardHeader,
-    CardBody,
-    CardFooter,
-} from "@material-tailwind/react";
+    Text,
+    Stack,
+    Image,
+    Input,
+    useDisclosure,
+    ModalCloseButton,
+  } from '@chakra-ui/react'
 
-function DialogWithForm() {
-    const [open, setOpen] = useState(false);
-    const handleOpen = () => setOpen((cur) => !cur);
+const DialogWithForm = () => {
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    const json = {
+        output: "Hello World"
+    }
+  return (
+    <div className='fixed bottom-4 right-4 z-50'>
+    <Button onClick={onOpen} >Open Modal</Button>
 
-    const [inputText, setInputText] = useState("");
-    const [reversedText, setReversedText] = useState("");
-    const [submittedText, setSubmittedText] = useState("");
+    <Modal blockScrollOnMount={false} isOpen={isOpen} onClose={onClose}>
+    <ModalOverlay />
+    <ModalContent>
+        <ModalHeader>Hindi to English</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+        <Stack direction="column" spacing={4} align="center">
+            <Image
+                src='https://images.unsplash.com/photo-1657302156083-2e61fb23d161?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2970&q=80'
+                alt='eng to hindi translate'
+                borderRadius='lg'
+            />
+            <form>
+                <Input placeholder="Translate" />
+            </form>
+            <Text marginTop={4} marginLeft={2} >{json.output}</Text>
+        </Stack>
+        </ModalBody>
 
-    // Clear input and output boxes when the dialog is initially opened
-    useEffect(() => {
-        if (open) {
-            setInputText("");
-            setReversedText("");
-            setSubmittedText("");
-        }
-    }, [open]);
-
-    const handleTranslate = async (e) => {
-        e.preventDefault(); // Prevent the default form submission behavior
-
-        try {
-            console.log("Sending request with inputText:", inputText);
-            const sending = {
-                input: inputText,
-            };
-            const jsonData = JSON.stringify(sending);
-
-            console.log(jsonData);
-
-            const response = await axios.post("http://127.0.0.1:5000/reverse", jsonData, {
-                headers: {
-                    "Content-Type": "application/json",
-                },
-            });
-
-            console.log("Response:", response);
-
-            if (response.status !== 200) {
-                throw new Error("Network response was not ok");
-            }
-
-            const data = response.data;
-            setReversedText(data.reversedText);
-            setSubmittedText(inputText); // Store the submitted input text
-        } catch (error) {
-            console.error("Error:", error);
-        }
-    };
-
-    return (
-        <>
-            <div className="fixed bottom-4 right-4 z-50">
-                <img
-                    src={menu}
-                    alt="Sign In"
-                    onClick={handleOpen}
-                    className="cursor-pointer h-9 w-9 sm:h-12 sm:w-12"
-                />
-                {open && <div className="fixed inset-0 backdrop-blur-sm bg-white opacity-60 blur flex items-center justify-center"></div>}
-                <Dialog
-                    size="xs"
-                    open={open}
-                    handler={handleOpen}
-                    className="bg-transparent shadow-none flex items-center justify-center"
-                >
-                    <Card className="h-full max-w-[30rem] rounded-md">
-                        <CardHeader className="p-4">
-                            <img
-                                src="https://images.unsplash.com/photo-1657302156083-2e61fb23d161?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2970&q=80"
-                                alt="Card Image"
-                                className="w-full rounded-2xl shadow-lg"
-                            />
-                        </CardHeader>
-                        <h1 className="text-2xl font-semibold text-center">English To हिंदी</h1>
-                        {/* <h3 className="text-center">(अंग्रेज़ी से हिंदी)</h3> */}
-                        <form onSubmit={handleTranslate} className="flex flex-col items-center"> {/* Move onSubmit here */}
-                            <CardBody className="gap-4">
-                                <input
-                                    type="text"
-                                    placeholder="Enter text"
-                                    value={inputText}
-                                    onChange={(e) => setInputText(e.target.value)}
-                                    className="w-[28rem] h-100 px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500"
-                                />
-                            </CardBody>
-                            <CardFooter className="pt-0">
-                                <button
-                                    type="submit"
-                                    className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 text-center w-100"
-                                >
-                                    Translate
-                                </button>
-                            </CardFooter>
-                        </form>
-                        {submittedText && (
-                            <div className="mt-2 px-4">
-                                <strong>Input:</strong>
-                                <div className="bg-gray-100 p-2 rounded-md mt-2">{submittedText}</div>
-                            </div>
-                        )}
-                        {reversedText && (
-                            <div className="mt-4 px-4 mb-4">
-                                <strong>Output:</strong>
-                                <div className="bg-gray-100 p-2 rounded-md mt-2">{reversedText}</div>
-                            </div>
-                        )}
-                    </Card>
-                </Dialog>
-            </div>
-        </>
-    );
+        <ModalFooter>
+        
+        <Button colorScheme='pink'>Submit</Button>
+        </ModalFooter>
+    </ModalContent>
+    </Modal>
+    </div>
+  )
 }
 
-export default DialogWithForm;
+export default DialogWithForm
