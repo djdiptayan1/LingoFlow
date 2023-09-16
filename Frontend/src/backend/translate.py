2a726acd9153eb1520a995a5ea2f5ca92689ba14
Diptayan Jash <111650788+djdiptayan1@users.noreply.github.com>
2023-09-16 11:01:01 +0530

    implemented FLASK backend. runs slow.

---
+++ Frontend/src/backend/translate.py
@@ -0,0 +1,12 @@
+from transformers import pipeline
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
+
+model = AutoModelForSeq2SeqLM.from_pretrained("Helsinki-NLP/opus-mt-en-hi")
+tokenizer = AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-en-hi")
+
+translation = pipeline("translation_en_to_hi", model=model, tokenizer=tokenizer)
+text = input('Enter the text to be translated: ')
+     
+
+translated_text = translation(text, max_length=500)[0]['translation_text']
+print(translated_text)
