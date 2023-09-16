1a3daa4eff054991bc2f21f588a36e9e1e40d3f8
Diptayan Jash <111650788+djdiptayan1@users.noreply.github.com>
2023-09-16 10:43:44 +0530

    Flask integrated with Translation

--- Frontend/src/backend/server.py
+++ Frontend/src/backend/server.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from transformers import pipeline
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
 
 app = Flask(__name__)
 CORS(app)
@@ -10,11 +12,18 @@ def reverse_text():
         data = request.get_json()
         # get the input text from the request
         input_text = data["input"]
-        
+
+        model = AutoModelForSeq2SeqLM.from_pretrained("Helsinki-NLP/opus-mt-en-hi")
+        tokenizer = AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-en-hi")
+        translation = pipeline("translation_en_to_hi", model=model, tokenizer=tokenizer)
+        translated_text = translation(input_text, max_length=500)[0]['translation_text']
+
         reversed_text = input_text[::-1]  # Reverse the input text
+        
+        
         print(f"Received Input text: {input_text}")  # Log the received input text
-        response_data = {"reversedText": reversed_text}
-        print(f"Reversed text: {reversed_text}")  # Log the reversed text
+        response_data = {"reversedText": translated_text}
+        print(f"Reversed text: {translated_text}")  # Log the reversed text
         return jsonify(response_data)
     except Exception as e:
         return jsonify({"error": str(e)}), 400
